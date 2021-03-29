995dd0c9d9fb72a692ffb1ff0e52bb2b2dc18a05
mayanksharmaufl <78172387+mayanksharmaufl@users.noreply.github.com>
2021-03-29 00:41:15 -0400

    Update HandShake.java

--- src/peer/HandShake.java
+++ src/peer/HandShake.java
@@ -1,171 +1,221 @@
 package peer;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.io.Serializable;
+import java.io.*;
 
-import utils.LogHandler;
+public class Handshake
+{
+	
+	// Attributes
+	//For Handshake message header
+	private static final int MSG_LENGTH = 32;
+	private static final int HANDSHAKE_HEADER_LENGTH = 18;
+	private static final int HANDSHAKE_ZEROBITS_LENGTH = 10;
+	private static final int HANDSHAKE_PEERID_LENGTH = 4;
+	private static final String CHARSET_NAME = "UTF8";
+	private static final String HANDSHAKE_HEADER = "P2PFILESHARINGPROJ";
+	private byte[] msg_header = new byte[HANDSHAKE_HEADER_LENGTH];
+	private byte[] peerID = new byte[HANDSHAKE_PEERID_LENGTH];
+	private byte[] zeroBits = new byte[HANDSHAKE_ZEROBITS_LENGTH];
+	private String messageHeader;
+	private String messagePeerID;
 
-public class HandShake implements Serializable {
 
-	/**
+	/* Class constructor
+	 * 
 	 * 
 	 */
-	private static final long serialVersionUID = -1482860868859618509L;
-	private static final String Header = "P2PFILESHARINGPROJ"; // fixed header show in description
-	private byte[] zeroBits = new byte[10];
-	private final String peerMsgHeader;
-	private String peerID; // self-peer ID 
-	private String targetPeerID; // the receiver of the msg
-  private boolean success; // success flag for handshake
+	public Handshake(String Header, String PeerId) {
 
-	private static LogHandler logging = new LogHandler();
-	private static SystemInfo sysInfo = SystemInfo.getSingletonObj();
+		try {
+			this.messageHeader = Header;
+			this.msg_header = Header.getBytes(CHARSET_NAME);
+			if (this.msg_header.length > HANDSHAKE_HEADER_LENGTH)
+				throw new Exception("Header too long");
+
+			this.messagePeerID = PeerId;
+			this.peerID = PeerId.getBytes(CHARSET_NAME);
+			if (this.peerID.length > HANDSHAKE_HEADER_LENGTH)
+				throw new Exception("Peer ID too long");
+
+			this.zeroBits = "0000000000".getBytes(CHARSET_NAME);
+		} catch (Exception e) {
+			//PeerProcess.showLog(e.toString());
+		}
+
+	}
+
+	public Handshake(){
+		
+	}
+
+	// return the handShakeHeader
+	public byte[] getHeader() {
+		return msg_header;
+	}
 	
-	/**
-	 * Client's handShake constructor
-	 * @param targetPeer - handshake msg receiver (Peer object)
-	 */
-	public HandShake(Peer targetPeer) {
-		super();
-		// Get self-peer info from SystemInfo Singleton -> self-peer Id & peer's neighborList;
-		this.peerID = sysInfo.getHostPeer().getId(); 
-		this.targetPeerID = targetPeer.getId();
-		this.peerMsgHeader = getHeader();
-		this.success = false;
+	// return the peerID
+	public byte[] getPeerID() {
+		return peerID;
 	}
 
-	/**
-	 * Server's handShake constructor
-	 * 1. Waiting client to send handshake
-	 * 2. Set targetPeerId by receiving msg
-	 */
-	public HandShake() {
-		super();
-		// Get self-peer info from SystemInfo Singleton -> self-peer Id & peer's neighborList;
-		this.peerID = sysInfo.getHostPeer().getId(); 
-		this.peerMsgHeader = getHeader();
-		this.targetPeerID = null;
-		this.success = false;
-	}
-
-	public byte[] getZeroBits() {
-		return zeroBits;
-	}
-
+	// Set the zeroBits 
 	public void setZeroBits(byte[] zeroBits) {
 		this.zeroBits = zeroBits;
 	}
 
-	/**
-	 * @return the peerID
-	 */
-	public String getPeerID() {
-		return peerID;
+	// return the zeroBits
+	public byte[] getZeroBits() {
+		return zeroBits;
 	}
 
-	/**
-	 * @param peerID
-	 * the peerID to set
-	 */
-	public void setPeerID(String peerID) {
-		this.peerID = peerID;
+	// return the messageHeader
+	public String getHeaderString() {
+		return messageHeader;
 	}
 
-	/**
-	 * @return the peerMsgHeader
-	 */
-	public String getPeerMsgHeader() {
-		return peerMsgHeader;
+	// return the messagePeerID
+	public String getPeerIDString() {
+		return messagePeerID;
 	}
 
-	/**
-	 * @return the header
-	 */
-	public static String getHeader() {
-		return Header;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
+	// Return the toString method of the Object
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		return sb.append("[Header :").append(getHeader()).append("]\n").append("[Zero Bits :").append(getZeroBits()).append("[Peer ID: ").append(this.peerID).append("]")
-				.toString();
+		return ("[HandshakeMessage] : Peer Id - " + this.messagePeerID
+				+ ", Header - " + this.messageHeader);
 	}
 
-	/**
-	 * HandShake step:
-	 * Client -> Server
-	 * Server response -> Client
-	 * @param out
-	 * @throws IOException
-	 */
-	public void SendHandShake(OutputStream out) throws IOException {
-		ObjectOutputStream opStream = new ObjectOutputStream(out);
-		opStream.writeObject(this);
-		opStream.flush();
-		logging.logSendHandShakeMsg(this.targetPeerID);
-	}
-
-	/**
-	 * Receive handshake string, get sender ID for response a handshake
-	 * @param in
-	 * @return sender ID 
-	 * @throws IOException
-	 */
-	public String ReceiveHandShake(InputStream in) throws IOException {
+	// Set the handShakeHeader
+	public void setHeader(byte[] handShakeHeader) {
 		try {
-			ObjectInputStream ipStream = new ObjectInputStream(in);
-			HandShake Response = (HandShake) ipStream.readObject();
-			// 1. TODO checkHeader
-			// 2. TODO check the its from the neighbor
-			// 3. Set the success flag to true after passing the tests above
-			setSuccess();
-			return Response != null ? Response.peerID : null;
-		} 
-		catch(ClassNotFoundException e){
-			logging.writeLog("severe", "read input stream exception, ex:" + e);
+			this.messageHeader = (new String(handShakeHeader, CHARSET_NAME)).toString().trim();
+			this.msg_header = this.messageHeader.getBytes();
+		} catch (UnsupportedEncodingException e) {
+			//PeerProcess.showLog(e.toString());
 		}
-		catch (Exception e) {
-			logging.writeLog("severe", String.format("Error, Receive handshake from [%s]", this.targetPeerID));
-			System.out.println(e);
-		}
-		return null;
 	}
 
 
-	// public boolean checkHeader() {
+	// Set the messagePeerID
+	public void setPeerID(String messagePeerID) {
+		try {
+			this.messagePeerID = messagePeerID;
+			this.peerID = messagePeerID.getBytes(CHARSET_NAME);
+		} catch (UnsupportedEncodingException e) {
+			//PeerProcess.showLog(e.toString());
+		}
+	}
 
-	// }
+	// Set the peerID 
+	public void setPeerID(byte[] peerID) {
+		try {
+			this.messagePeerID = (new String(peerID, CHARSET_NAME)).toString().trim();
+			this.peerID = this.messagePeerID.getBytes();
 
-	// public boolean checkPeerId(msgPeerId) {
-	// 	Peer p = SystemInfo.getSingletonObj().getHostPeer();
-	// }
-
-	// public boolean isNeighbor(peerId) {
-
-	// }
+		} catch (UnsupportedEncodingException e) {
+			//PeerProcess.showLog(e.toString());
+		}
+	}
 	
-	/**
-	 * Passing checkList.
-	 * The handshake is success.
-	 */
-	private void setSuccess() {
-		this.success = true;
+	// Set the messageHeader 
+	public void setHeader(String messageHeader) {
+		try {
+			this.messageHeader = messageHeader;
+			this.msg_header = messageHeader.getBytes(CHARSET_NAME);
+		} catch (UnsupportedEncodingException e) {
+			//PeerProcess.showLog(e.toString());
+		}
+	}
+	
+	// Decodes the byte array HandshakeMessage and loads to the object HandshakeMessage
+	public static Handshake decodeMessage(byte[] receivedMessage) {
+
+		Handshake handshakeMessage = null;
+		byte[] msgHeader = null;
+		byte[] msgPeerID = null;
+
+		try {
+			// Initial check
+			if (receivedMessage.length != MSG_LENGTH)
+				throw new Exception("Unmatched byte array length");
+
+			// VAR initialization
+			handshakeMessage = new Handshake();
+			msgHeader = new byte[HANDSHAKE_HEADER_LENGTH];
+			msgPeerID = new byte[HANDSHAKE_PEERID_LENGTH];
+
+			// Decode the received message
+			System.arraycopy(receivedMessage, 0, msgHeader, 0,
+					HANDSHAKE_HEADER_LENGTH);
+			System.arraycopy(receivedMessage, HANDSHAKE_HEADER_LENGTH
+					+ HANDSHAKE_ZEROBITS_LENGTH, msgPeerID, 0,
+					HANDSHAKE_PEERID_LENGTH);
+
+			// Populate handshakeMessage entity
+			handshakeMessage.setHeader(msgHeader);
+			handshakeMessage.setPeerID(msgPeerID);
+
+		} catch (Exception e) {
+			//PeerProcess.showLog(e.toString());
+			handshakeMessage = null;
+		}
+		return handshakeMessage;
 	}
 
-	/**
-	 * return if handshake is success or not
-	 */
-	public boolean isSuccess() {
-		return this.success;
+	// Encodes a given message in the format HandshakeMessage 
+	public static byte[] encodeMessage(Handshake handshakeMessage) {
+
+		byte[] sendMessage = new byte[MSG_LENGTH];
+
+		try {
+			// Encode header
+			if (handshakeMessage.getHeader() == null) {
+				throw new Exception("Header Invalid");
+			}
+			if (handshakeMessage.getHeader().length > HANDSHAKE_HEADER_LENGTH|| handshakeMessage.getHeader().length == 0)
+			{
+				throw new Exception("Header Invalid");
+			} else {
+				System.arraycopy(handshakeMessage.getHeader(), 0, sendMessage,
+						0, handshakeMessage.getHeader().length);
+			}
+
+			// Encode zero bits
+			if (handshakeMessage.getZeroBits() == null) {
+				throw new Exception("Invalid zero bits field.");
+			} 
+			if (handshakeMessage.getZeroBits().length > HANDSHAKE_ZEROBITS_LENGTH
+					|| handshakeMessage.getZeroBits().length == 0) {
+				throw new Exception("Invalid zero bits field.");
+			} else {
+				System.arraycopy(handshakeMessage.getZeroBits(), 0,
+						sendMessage, HANDSHAKE_HEADER_LENGTH,
+						HANDSHAKE_ZEROBITS_LENGTH - 1);
+			}
+
+			// Encode peer id
+			if (handshakeMessage.getPeerID() == null) 
+			{
+				throw new Exception("Invalid peerID");
+			} 
+			else if (handshakeMessage.getPeerID().length > HANDSHAKE_PEERID_LENGTH
+					|| handshakeMessage.getPeerID().length == 0) 
+			{
+				throw new Exception("Invalid peerID");
+			} 
+			else 
+			{
+				System.arraycopy(handshakeMessage.getPeerID(), 0, sendMessage,
+						HANDSHAKE_HEADER_LENGTH + HANDSHAKE_ZEROBITS_LENGTH,
+						handshakeMessage.getPeerID().length);
+			}
+
+		} 
+		catch (Exception e) 
+		{
+			//PeerProcess.showLog(e.toString());
+			sendMessage = null;
+		}
+
+		return sendMessage;
 	}
 }
