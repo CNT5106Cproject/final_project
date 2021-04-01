3cd961555b30e11874f84921260f2d6dfa8eb37b
YiMing <ericdreamwithvic@gmail.com>
2021-04-01 15:26:30 -0400

    [fix] remove the warnings

--- src/peer/PieceMsg.java
+++ src/peer/PieceMsg.java
@@ -1,5 +1,9 @@
 package peer;
 public class PieceMsg extends ShortPayloadMsg{
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 8683107767721716599L;
 	private byte[] data;
 	public void setData(int msgLen, byte msgType, int blockIdx, byte[] data){
 		this.msgLen = msgLen;
