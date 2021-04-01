dd1c5653ecfd87070449096014a849c8eaf92b36
YiMing <ericdreamwithvic@gmail.com>
2021-04-01 15:29:17 -0400

    [feature] adding timers into server
    1. PreferSelect -> select k interest nodes for file deliver
    2. [TODO] OptSelect on the chokingMap

--- src/peer/Server.java
+++ src/peer/Server.java
@@ -4,6 +4,16 @@ import java.net.*;
 import java.io.*;
 import java.nio.*;
 import java.nio.channels.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Map.Entry;
+import java.util.concurrent.locks.ReentrantLock;
 
 import utils.CustomExceptions;
 import utils.LogHandler;
@@ -15,14 +25,11 @@ public class Server extends Thread{
 	private static SystemInfo sysInfo = SystemInfo.getSingletonObj();
 	private static FileManager fm = FileManager.getInstance();
 	private static LogHandler logging = new LogHandler();
-
+	private Timer PreferSelectTimer = new Timer();
+	private Timer OptSelectTimer = new Timer();
+	
 	public Server() {
-		Peer hostPeer = sysInfo.getHostPeer();
-		this.hostPeer = hostPeer;
-	}
-
-	public Server(Peer peer) {
-		this.hostPeer = peer;
+		this.hostPeer = sysInfo.getHostPeer();
 	}
 
 	public void run() {
@@ -31,7 +38,17 @@ public class Server extends Thread{
 		try {
 			ServerSocket listener = new ServerSocket(hostPeer.getPort());
 			try {
-				int clientNum = 1;
+				
+				/**
+				 * Set up the unckoking & opt peer selection mechanism
+				 */
+				logging.writeLog("Establishing Timer for PreferSelect with interval: " + sysInfo.getUnChokingInr()*1000 + "(ms)");
+				PreferSelect taskPrefSelect = new PreferSelect();
+				// OptSelect optSelect = new OptSelect();
+				this.PreferSelectTimer.schedule(taskPrefSelect, 0, sysInfo.getUnChokingInr()*1000);
+				// this.OptSelectTimer.schedule(optSelect, sysInfo.getOptUnChokingInr());
+
+				int clientNum = 0;
 				while(true) {
 					new Handler(listener.accept(), clientNum, this.hostPeer).start();
 					// System.out.println("Client "  + clientNum + " is connected!");
@@ -53,10 +70,154 @@ public class Server extends Thread{
 	}
 
 	/** 
-	 *  This class will set an interval timer to make the unchoking decision.
+	 *  This class will set an interval timer to make the unchoking-choking decision.
 	*/
-	private static class ChokingMechanism extends Thread {
+	public static class PreferSelect extends TimerTask {
+		/**
+    * UnchokingInterval p
+    * - NumberOfPreferredNeighbors k
+    */
+		private final ReentrantLock lock = new ReentrantLock();
+		private final static int preferN = sysInfo.getPreferN();
+		ArrayList<Entry<String, Peer>> bestNeighbors;
+		private ActualMsg actMsg;
+
+		/**
+		 * Select k neighbors from the interesting list
+		 */
+		private HashMap<String, Peer> neighborMap = sysInfo.getNeighborMap();;
+		private HashMap<String, Peer> interestMap = sysInfo.getInterestMap();
+		private HashMap<String, Peer> previousChokeMap = null;
+
+		private boolean randomlySelect = false;
+		Random R = new Random();
+
+		PreferSelect() {
+			this.actMsg = new ActualMsg();
+		}
+
+		/**
+		 * 1. update neighbor map
+		 * 2. update interest map 
+		 * 3. random select | picked by download rate
+		 * 4. update chokelist, send choke & unchoke message
+		 */
+		public void run() {
+			logging.writeLog("Start PreferSelect - selecting the nodes to pass file pieces");
+			if(sysInfo.getHostPeer().getHasFile() || (fm != null && fm.isComplete())) {
+				randomlySelect = true;
+			}
+			
+			testRandomDownLoadRate();
+
+			for(Entry<String, Peer> n: neighborMap.entrySet()) {
+				if(n.getValue().getIsInterested()) {
+					interestMap.put(n.getKey(), n.getValue());
+				}
+				else if(!n.getValue().getIsInterested()) {
+					interestMap.remove(n.getKey());
+				}
+			}
+
+			/**
+			 * Pick k interested neighbors
+			 * 3. random select | picked by download rate
+			 */
+			if(randomlySelect) {
+				logging.writeLog("selecting interested node randomly");
+				if(!interestMap.isEmpty()) {
+					List<Entry<String, Peer>> interestList = sortHashMapByDownload(interestMap);
+					for(Entry<String, Peer> i: interestList) {
+						logging.writeLog(String.format("%s download rate: %s", i.getKey(), i.getValue().getDownloadRate()));
+					}
+				}
+			}
+			else {
+				logging.writeLog("selecting interested node by download rate");
+				if(!interestMap.isEmpty()) {
+					// Sort the interested node in order of the download rate 
+					logging.writeLog("[IMPORTANT] set interestList in order by download rate");
+					List<Entry<String, Peer>> interestList = sortHashMapByDownload(interestMap);
+					for(Entry<String, Peer> i: interestList) {
+						logging.writeLog(String.format("%s download rate: %s", i.getKey(), i.getValue().getDownloadRate()));
+					}
+					
+					previousChokeMap = sysInfo.getChokingMap();
+					sysInfo.clearChokingMap();
+					int count = 1;
+				
+					for(Entry<String, Peer> i: interestList) {
+						if(count > preferN) {
+							// Not been picked, choke them
+							previousChokeMap.put(i.getKey(), i.getValue());
+						}
+						else {
+							// check if peer is choked in previous round;
+							if(previousChokeMap.get(i.getKey()) == null) {
+								// send unchoke;
+								// this.actMsg.send(outConn, ActualMsg.BITFIELD, fm.getOwnBitfield());
+							}
+						}
+						count ++;
+					}
+				}
+			}
+		}
+
+		private List<Entry<String, Peer>> sortHashMapByDownload(HashMap<String, Peer> targetMap) {
+			logging.writeLog("start sortHashMapByDownload with targetMap size: " + targetMap.size());
+			if(targetMap.size() > 0) {
+				List<Entry<String, Peer>> targetList = new ArrayList<Entry<String, Peer>>(targetMap.entrySet());
+				
+				/**
+				 * Compare the peer objects downloading rate
+				 */
+				Comparator <Entry<String, Peer>> rate = new Comparator<Entry<String, Peer>>() {
+					@Override
+					public int compare(Entry<String, Peer> p1, Entry<String, Peer> p2) {
+						double r1 = p1.getValue().getDownloadRate();
+						double r2 = p2.getValue().getDownloadRate();
+
+						/**
+						 * In descending order.
+						 */
+						if(r1 < r2) return 1;
+						if(r1 > r2) return -1;
+						return 0;
+					}
+				};
+				
+				Collections.sort(targetList, rate);
+				return targetList;
+			}
+			return null;
+		}
 		
+		/**
+		 * Assign Random download rate to nodes
+		 */
+		private void testRandomDownLoadRate() {	
+			for(Entry<String, Peer> n: neighborMap.entrySet()) {
+				n.getValue().setDownloadRate(R.nextDouble());
+			}
+		}
+	}
+
+	/** 
+	*  This class will set an interval timer to make the opt unchoking decision.
+	*/
+	private static class OptSelect extends TimerTask {
+		private final ReentrantLock lock = new ReentrantLock();
+		// private final static int optUnchokingInr = sysInfo.getOptUnChokingInr();
+  	private final static int optCount = 1;
+		Random R = new Random();
+		/**
+		* OptimisticUnchokingInterval 10
+		* - 1 optimistically unchoked neighbor
+    */
+		public void run() {
+
+		}
 	}
 
 	/**
@@ -169,7 +330,7 @@ public class Server extends Thread{
 		 * @param peerId
 		 * @param status
 		 */
-		private void setNeighborIN(String peerId, boolean status) {
+		private synchronized void setNeighborIN(String peerId, boolean status) {
 			Peer p = sysInfo.getNeighborMap().get(peerId);
 			p.setIsInterested(status);
 			sysInfo.getNeighborMap().put(peerId, p);
