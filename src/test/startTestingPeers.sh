d9890598147ca3a30cde9a1ab4c5da29768873ab
YiMing <ericdreamwithvic@gmail.com>
2021-04-01 15:27:57 -0400

    [test] adding third process

--- src/test/startTestingPeers.sh
+++ src/test/startTestingPeers.sh
@@ -2,4 +2,5 @@
 echo "${PWD}"
 java -Duser.language=en -cp ../../lib peer.PeerProcess 1001 &
 java -Duser.language=en -cp ../../lib peer.PeerProcess 1002 &
+java -Duser.language=en -cp ../../lib peer.PeerProcess 1003 &
 echo "Deployed all peers succesfully."
