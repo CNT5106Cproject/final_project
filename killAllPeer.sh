b62742cfd492a6ffd67c9afc788ee1ff363e52ef
YiMing <ericdreamwithvic@gmail.com>
2021-04-13 15:22:28 -0400

    [script] add remove all file, add kill cmd in ubuntu

--- killAllPeer.sh
+++ killAllPeer.sh
@@ -2,4 +2,4 @@
 kill -9 $(ps aux | grep java | grep peer.PeerProces | awk '{print $2}')
 
 # Use for remote server demo
-# kill $(ps aux | grep java | grep peer.PeerProces | grep yiming | awk '{print $2}')
+# ps aux | grep java | grep peer.PeerProces | grep yiming | awk '{print $2}' | xargs /bin/kill
---
+++ rmAllFile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "${PWD}"
+
+rm ./project/1002/thefile
+rm ./project/1003/thefile
+rm ./project/1004/thefile
+rm ./project/1005/thefile
+rm ./project/1007/thefile
+rm ./project/1008/thefile
+rm ./project/1009/thefile
+echo "Remove all file in project succesfully."
