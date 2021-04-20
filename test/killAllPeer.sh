00dedb850725195528fb501abcb35e80f288b190
YiMing <ericdreamwithvic@gmail.com>
2021-04-19 23:02:48 -0400

    [file] set up kill all remote java

--- test/killAllPeer.sh
+++ test/killAllPeer.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
-kill -9 $(ps aux | grep java | grep PeerProces | awk '{print $2}')
+#!/bin/bash mac local
+# kill -9 $(ps aux | grep java | grep PeerProces | awk '{print $2}')
 
 # Use for remote server demo
-# ps aux | grep java | grep PeerProces | grep yiming | awk '{print $2}' | xargs /bin/kill
+ssh lin114-00 ps aux | grep java | grep PeerProces | grep yiming | awk '{print $2}' | xargs /bin/kill;
+ssh lin114-01 ps aux | grep java | grep PeerProces | grep yiming | awk '{print $2}' | xargs /bin/kill;
+ssh lin114-02 ps aux | grep java | grep PeerProces | grep yiming | awk '{print $2}' | xargs /bin/kill;
+ssh lin114-03 ps aux | grep java | grep PeerProces | grep yiming | awk '{print $2}' | xargs /bin/kill;
+ssh lin114-04 ps aux | grep java | grep PeerProces | grep yiming | awk '{print $2}' | xargs /bin/kill;
+ssh lin114-05 ps aux | grep java | grep PeerProces | grep yiming | awk '{print $2}' | xargs /bin/kill;
+ssh lin114-06 ps aux | grep java | grep PeerProces | grep yiming | awk '{print $2}' | xargs /bin/kill;
+ssh lin114-07 ps aux | grep java | grep PeerProces | grep yiming | awk '{print $2}' | xargs /bin/kill;
+ssh lin114-08 ps aux | grep java | grep PeerProces | grep yiming | awk '{print $2}' | xargs /bin/kill;
